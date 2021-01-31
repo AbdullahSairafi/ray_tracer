71729a04065e5b2569f67142b591a6987f1f0dac
Abdullah Sairafi <abdallah.sairafi@gmail.com>
2021-01-31 13:38:15 +0300

    fixed some issues, extending camera class

--- obj/linux64/Release/src/vector.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Release/src/vector.o: \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/vector.cpp \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/vector.h
-
-/home/sairafaa/Desktop/projects/cpp/ray_tracer/src/vector.h:
