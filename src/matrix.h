fd34d95db80c4601809a2d31bdb532262e15ecfe
Abdullah Sairafi <abdallah.sairafi@gmail.com>
2021-02-03 21:38:08 +0300

    added most of matrix operations

--- src/matrix.h
+++ src/matrix.h
@@ -20,25 +20,32 @@ public:
     int get_cols() const {return m_cols;}
 
     // overloaded assignment operator
-    const Matrix& operator=(const Matrix &rhs);
+    Matrix<T>& operator=(const Matrix<T> &rhs);
 
     // matrix matrix operations
-    friend Matrix operator+(const Matrix &m1, const Matrix &m2);
-    friend Matrix operator-(const Matrix &m1, const Matrix &m2);
-    friend Matrix operator*(const Matrix &m1, const Matrix &m2);
-    friend Matrix matmul(const Matrix &m1, const Matrix &m2);
+    friend Matrix<T> operator+(const Matrix<T> &m1, const Matrix<T> &m2);
+    friend Matrix<T> operator-(const Matrix<T> &m1, const Matrix<T> &m2);
+    friend Matrix<T> operator*(const Matrix<T> &m1, const Matrix<T> &m2);
+    friend Matrix<T> matmul(const Matrix<T> &m1, const Matrix<T> &m2);
+
+    //overloaded +=, -=, *=
+    Matrix<T>& operator+=(const Matrix<T> &rhs);
+    Matrix<T>& operator-=(const Matrix<T> &rhs);
+    Matrix<T>& operator*=(const Matrix<T> &rhs);
 
     // matrix scalar operations
-    friend Matrix operator*(const Matrix &m, const T &c);
-    friend Matrix operator*(const T &c, const Matrix &m);
+    friend Matrix<T> operator*(const Matrix<T> &m, const T &c);
+    friend Matrix<T> operator*(const T &c, const Matrix<T> &m);
 
     // matrix vector operations
-    friend vector<T> operator*(const Matrix &m, const vector<T> &v);
-    friend vector<T> operator*(const vector<T> &v, const Matrix &m);
-    friend vecmul(const vector<T> &v, const Matrix &m);
-    friend vecmul(const Matrix &m, const vector<T> &v);
+    friend vector<T> operator*(const Matrix<T> &m, const vector<T> &v);
+    friend vector<T> operator*(const vector<T> &v, const Matrix<T> &m);
+    friend vecmul(const vector<T> &v, const Matrix<T> &m);
+    friend vecmul(const Matrix<T> &m, const vector<T> &v);
 
     // overload access operator()
+    T& operator()(int i, int j);
+    const T& operator()(int i, int j) const;
 
 private:
     int m_rows;
@@ -62,7 +69,7 @@ Matrix<T>::Matrix(const Matrix &rhs){
 }
 
 template <class T> 
-Matrix& Matrix<T>::operator=(const Matrix &rhs){
+Matrix<T>& Matrix<T>::operator=(const Matrix &rhs){
    if(this == &rhs){ // avoid self-assignment
         return *this;
     }
@@ -79,17 +86,88 @@ Matrix& Matrix<T>::operator=(const Matrix &rhs){
 }
 
 template <class T>
-Matrix Matrix<T>::operator+(const Matrix &m1, const Matrix &m2){
+Matrix<T> Matrix<T>::operator+(const Matrix &m1, const Matrix &m2){
     // make sure m1 and m2 are compatible for addition
     assert(m1.get_rows() == m2.get_rows() && m1.get_cols() == m2.get_cols());
     // 0.0 will be typecaseted to the proper type.
-    Matrix result{m1.get_rows(), m1.get_cols(), 0.0};
-    for(int i = 0; i < m1.get_rows(); i++{
-        for(int j = 0; j < m1.get_cols(); j++){
+    Matrix<T> result{m1.get_rows(), m1.get_cols(), 0.0};
+    for(int i = 0; i < result.get_rows(); i++{
+        for(int j = 0; j < result.get_cols(); j++){
             result(i,j) = m1(i,j) + m2(i,j);
         }
     }
     return result;
 }
 
+template <class T>
+Matrix<T> Matrix<T>::operator-(const Matrix<T> &m1, const Matrix<T> &m2){
+    // make sure m1 and m2 are compatible for subtraction
+    assert(m1.get_rows() == m2.get_rows() && m1.get_cols() == m2.get_cols());
+    Matrix<T> result{m1.get_rows(), m1.get_cols(), T{}};
+    for(int i = 0; i < result.get_rows(); i++{
+        for(int j = 0; j < result.get_cols(); j++){
+            result(i,j) = m1(i,j) - m2(i,j);
+        }
+    }
+    return result;
+}
+
+template <class T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T> &m1, const Matrix<T> &m2){
+    // make sure m1 and m2 are compatible for matrix multiplication
+    assert(m1.get_cols() == m2.get_rows());
+    Matrix<T> result{m1.get_rows, m2.get_cols(), T{}};
+    for(int i = 0; i < result.get_rows(); i++){
+        for(int j = 0; j < result.get_cols(); j++){
+            for(int k; k < m1.get_cols(); k++){
+                result(i,j) += m1(i,k) * m2(k,j);
+            }
+        }
+    }
+    return result;
+}
+
+template <class T>
+Matrix<T> Matrix<T>::matmul(const Matrix<T> &m1, const Matrix<T> &m2){
+    return m1 * m2;
+}
+
+
+template <class T>
+Matrix<T>& Matrix<T>::operator+=(const Matrix<T> &rhs){
+    *this = *this + rhs;
+    return *this;
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator-=(const Matrix<T> &rhs){
+    *this = *this - rhs;
+    return *this;
+}
+
+template <class T>
+Matrix<T>& Matrix<T>::operator*=(const Matrix<T> &rhs){
+    *this = *this * rhs;
+    return *this;
+}
+
+
+template <class T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T> &m, const T &c){
+    int rows = m.get_rows();
+    int cols = m.get_cols();
+    Matrix<T> result{rows, cols, T{}};
+    
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            result(i,j) = m(i,j) * c;
+        }
+    }
+    return result;
+}
+
+template <class T>
+Matrix<T> Matrix<T>::operator*(const T &c, const Matrix<T> &m){
+    return m * c;
+}
 #endif
