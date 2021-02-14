ce8ccbab3ca040d11d9ffe8c50dc9d8a9ec098b9
Abdullah Sairafi <abdallah.sairafi@gmail.com>
2021-02-14 21:02:18 +0300

    added trinagle class

--- obj/linux64/Release/src/color.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Release/src/color.o: \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/color.cpp \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/color.h
-
-/home/sairafaa/Desktop/projects/cpp/ray_tracer/src/color.h:
--- obj/linux64/Release/src/ray.d
+++
@@ -1,8 +0,0 @@
-obj/linux64/Release/src/ray.o: \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/ray.cpp \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/ray.h \
- /home/sairafaa/Desktop/projects/cpp/ray_tracer/src/vector.h
-
-/home/sairafaa/Desktop/projects/cpp/ray_tracer/src/ray.h:
-
-/home/sairafaa/Desktop/projects/cpp/ray_tracer/src/vector.h:
