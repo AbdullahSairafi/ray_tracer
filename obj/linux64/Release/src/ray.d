0b3c1d88b0a935f096f402bf4be36054cc387727
AbdullahSairafi <abdallah.sairafi@gmail.com>
2021-03-04 17:16:23 +0300

    removed grep

--- obj/linux64/Release/src/ray.d
+++
@@ -1,8 +0,0 @@
-obj/linux64/Release/src/ray.o: \
- /home/as/Desktop/projects/cpp/ray_tracer/src/ray.cpp \
- /home/as/Desktop/projects/cpp/ray_tracer/src/ray.h \
- /home/as/Desktop/projects/cpp/ray_tracer/src/vector.h
-
-/home/as/Desktop/projects/cpp/ray_tracer/src/ray.h:
-
-/home/as/Desktop/projects/cpp/ray_tracer/src/vector.h:
